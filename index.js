981d39df91fae3442fcfcd1556a37e79d2562aa0
Carlos Eduardo <cers.405@gmail.com>
2025-03-20 21:17:53 -0300

    Alterção

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let resultado = diminuir(10, 2)//entre parenteses o numero de vitorias seguido do numero de derrotas do jogador
-
-console.log("O número de vitórias é " + resultado)
+let resultado = diminuir(85, 2)//entre parenteses o numero de vitorias seguido do numero de derrotas do jogador
 
 function diminuir(vitorias, derrotas){
 	let diminuicao = vitorias - derrotas
     return diminuicao
 }
 
-if (resultado <=10){
+if (resultado <= 10){
     console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Ferro")
+}
+else if (resultado >= 11 && resultado <= 20) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Bronze")
+}
+else if (resultado >= 21 && resultado <= 50) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Prata")
+}
+else if (resultado >= 51 && resultado <= 80) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Ouro")
+}
+else if (resultado >= 81 && resultado <= 90) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Diamante")
+}
+else if (resultado >= 91 && resultado <= 100) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Lendário")
+}
+else if (resultado >= 101) {
+    console.log("O Herói tem saldo de " + resultado + " vitórias e está no nível Imortal")
 }
